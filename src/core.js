d866f888a929cfb904249a5db267149c7494a144
YOSHIKI <vicugnapacos.g@gmail.com>
2021-01-01 17:38:01 +0900

    調整

--- src/core.js
+++ src/core.js
@@ -1,5 +1,5 @@
-import player from "./modules/demo-player";
 import dataurlConverter from "./modules/dataurl-converter";
+import player from "./modules/demo-player";
 
 export default class {
   constructor() {
@@ -8,31 +8,46 @@ export default class {
       return false;
     }
 
-    this.player = new player();
     this.dataurlConverter = new dataurlConverter();
-    this.dropArea = document.getElementById('dropArea');
+    this.player = new player();
 
     this.dragEvents();
   }
   dragEvents() {
-    let self = this;
-    self.dropArea.addEventListener('dragover', function(e) {
+    let self = this,
+        dropArea = document.getElementById('dropArea');
+
+    dropArea.addEventListener('dragover', function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      e.dataTransfer.dropEffect = 'copy';
+
+      if(dropArea.classList.contains('active')) return;
+      dropArea.classList.add('active');
+    });
+    dropArea.addEventListener('dragleave', function(e) {
       e.stopPropagation();
       e.preventDefault();
 
+      if(!dropArea.classList.contains('active')) return;
+			dropArea.classList.remove('active');
     });
-    self.dropArea.addEventListener('drop', function(e) {
+
+    dropArea.addEventListener('drop', function(e) {
       e.stopPropagation();
       e.preventDefault();
 
       let files = e.dataTransfer.files;
-      self.dataurlConverter.convert(files).then(function(res) {
-        console.log(res);
-        self.player.play(res);
-      }, function(error) {
-        console.log(error);
-      });
+      self.dropFile(files);
 
+    });
+  }
+  dropFile(files) {
+    let self = this;
+    self.dataurlConverter.convert(files).then(function(res) {
+      self.player.play(res);
+    }, function(error) {
+      console.log(error);
     });
   }
 }
